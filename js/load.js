7625c261a78502f1748bcb110779c108ab273e61
Fery Irawan <fery21irawan.me@gmail.com>
2021-09-07 21:35:39 +0700

    update username fery21irawan to feryajwa

--- js/load.js
+++ js/load.js
@@ -8,16 +8,16 @@
 
 var socialtags = [{
     img: "img/039-github.svg",
-    link: "https://github.com/fery21irawan"
+    link: "https://github.com/feryajwa"
 }, {
     img: "img/031-linkedin.svg",
-    link: "https://www.linkedin.com/in/fery21irawan/"
+    link: "https://www.linkedin.com/in/feryajwa/"
 }, {
     img: "img/034-instagram.svg",
-    link: "https://instagram.com/fery21irawan"
+    link: "https://instagram.com/feryajwa"
 }, {
     img: "img/045-facebook.svg",
-    link: "https://www.facebook.com/fery21irawan"
+    link: "https://www.facebook.com/feryajwa"
 }]
 
 function loadSocialIcons() {
