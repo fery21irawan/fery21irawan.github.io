eef0d027585ce275067c6b0f4c674538640bc0a8
ferys2195 <fery21irawan.me@gmail.com>
2024-03-18 21:00:39 +0700

    add all (masih stuck dengan pekerjaan lain)

---
+++ src/data/certificates.ts
@@ -0,0 +1,37 @@
+const certificates = [
+	{
+		title: 'Front End Web',
+		datalist: [
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+		],
+	},
+	{
+		title: 'Data Science & Machine Learning',
+		datalist: [
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+		],
+	},
+	{
+		title: 'Android',
+		datalist: [
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+		],
+	},
+	{
+		title: 'Others',
+		datalist: [
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+			'../public/certificates/11-Menjadi Front End Web Developer Expert-page-00001.jpg',
+		],
+	},
+]
+
+export { certificates }
---
+++ src/data/profile.ts
@@ -0,0 +1,5 @@
+export default {
+  name: 'Fery Irawan',
+  imageUrl: 'https://avatars.githubusercontent.com/u/20228547?v=4',
+  jobs: 'Software Developer',
+}
